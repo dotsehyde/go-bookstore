c36e7785a857550b635ac75f76f8c3995fa73153
Benjamin <dotsehyde@gmail.com>
2023-02-25 13:13:46 +0000

    ✨ feat:Added update book endpoint

--- controller/book.controller.go
+++ controller/book.controller.go
@@ -7,13 +7,20 @@ import (
 	"strconv"
 
 	"github.com/dotsehyde/models"
+	"github.com/dotsehyde/utils"
 	"github.com/gorilla/mux"
 )
 
 var NewBook models.Book
 
 func CreateBook(w http.ResponseWriter, r *http.Request) {
-
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func GetAllBooks(w http.ResponseWriter, r *http.Request) {
@@ -39,9 +46,41 @@ func GetSingleBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateBook(w http.ResponseWriter, r *http.Request) {
-
+	var UpdateBook = &models.Book{}
+	utils.ParseBody(r, UpdateBook)
+	vars := mux.Vars(r)
+	bookId := vars["id"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	bookDetails, db := models.GetSingleBook(ID)
+	if UpdateBook.Name != "" {
+		bookDetails.Name = UpdateBook.Name
+	}
+	if UpdateBook.Author != "" {
+		bookDetails.Author = UpdateBook.Author
+	}
+	if UpdateBook.Publication != "" {
+		bookDetails.Publication = UpdateBook.Publication
+	}
+	db.Save(&bookDetails)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func DeleteBook(w http.ResponseWriter, r *http.Request) {
-
+	vars := mux.Vars(r)
+	bookId := vars["id"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
